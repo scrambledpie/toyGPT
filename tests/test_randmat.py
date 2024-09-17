b638e7c94507b46713f1ad64cdb488aa9657eb6c
Michael Pearce <scrambledpie@gmail.com>
2024-09-17 18:12:42 +0000

    random integer matrix generator

--- tests/test_randmat.py
+++ tests/test_randmat.py
@@ -14,9 +14,20 @@ def test_random_fixed_key():
     """ subsequent calls with fixed key are the same """
     Random.key = jax.random.key(0)
     mat_1 = Random.randmat((10, 10))
+    mat_11 = Random.randint((10, 10))
 
     Random.key = jax.random.key(0)
     mat_2 = Random.randmat((10, 10))
+    mat_22 = Random.randint((10, 10))
+
     assert (mat_1 == mat_2).sum() == 100
+    assert (mat_11 == mat_22).sum() == 100
 
 
+def test_randint():
+    matrix = Random.randint(shape=(10, 10), minval=100, maxval=105)
+    assert matrix.shape == (10, 10)
+    assert (jax.numpy.round(matrix) == matrix).sum() == 100
+
+    assert 100 <= matrix.min()
+    assert matrix.max() <= 105
--- toyGPT/randmat.py
+++ toyGPT/randmat.py
@@ -15,6 +15,9 @@ class Random:
         minval:float=0,
         maxval:float=1,
     ) -> jnp.ndarray:
+        """
+        Sample random uniform matrix
+        """
         cls.key, subkey = jax.random.split(cls.key)
         matrix = jax.random.uniform(
             minval=minval,
@@ -24,3 +27,24 @@ class Random:
             dtype=dtype,
         )
         return matrix
+
+    @classmethod
+    def randint(
+        cls,
+        shape:list[int],
+        dtype=jnp.int32,
+        minval:float=0,
+        maxval:float=10000,
+    ):
+        """
+        Sample random integer matrix
+        """
+        cls.key, subkey = jax.random.split(cls.key)
+        shape = tuple(shape) + (maxval - minval,)
+        matrix = minval + jax.random.categorical(
+            key=subkey,
+            logits=jnp.ones(shape=shape, dtype=jnp.float16),
+            axis=-1,
+        ).astype(dtype)
+        return matrix
+
